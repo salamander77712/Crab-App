10dce010ca07bae3566bc01d4ccb479fb9840fdd
PickleSalamander <xander.h.wright@gmail.com>
2025-01-14 12:13:08 -0500

    Initial commit

---
+++ components/CrabCatchButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const crabs:Ref = useState('crabs')
+</script>
+
+<template>
+<button @click="crabs++">Catch Crabs</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/CrabManager.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const crabs = useState('crabs', () => 0)
+</script>
+
+<template>
+<p>You have {{ crabs }} crabs</p>
+<CrabCatchButton />
+</template>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
